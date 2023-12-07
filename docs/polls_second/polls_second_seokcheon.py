727dfbf102077d2553226d1cf48d1216bd2366a7
seokcheon <my980519@gmail.com>
2023-12-07 10:29:52 +0900

    제꺼입니다.

--- docs/polls_second/polls_second_seokcheon.py
+++ docs/polls_second/polls_second_seokcheon.py
@@ -22,6 +22,8 @@ list_question = [
 
 list_answer =  ["좋음", "중간", "좋아지길"]
 
+list_statistics = [0,0,0]
+list_score = [3,2,1]
 for num_first in [0,1,2,3]:
     # str_question = list_question[num_first] 
     print("{}. {}".format(num_first+1, list_question[num_first]))
@@ -30,9 +32,22 @@ for num_first in [0,1,2,3]:
         # str_answer = list_answer[num_second]
         print(" {}. {}".format(num_second+1, list_answer[num_second]),end="")
     print("")
+
+    str_question_result = input("당신 생각은 몇 번 : ")
+    num_question_result = int(str_question_result)
+    index = num_question_result - 1
+    list_statistics[index]=list_statistics[index]+1
+
     if num_first < 3 :
         pass 
         print("----------")
     else :
         print("")
     
+
+print("설문자 답항별 갯수 표시 : ({},".format(list_statistics))
+
+print(list_score[index])
+
+print("답항 가중 평균 : {}+{}+{}/ 3+2+1 =".format(list_statistics*list_score[index]),list_statistics*list_score[index],list_statistics*list_score[index])
+
